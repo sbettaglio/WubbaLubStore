23e8b806bde283cab5b53766ee11ace09abc07a6
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-04 16:18:20 -0500

    Get for all location and Post for new locatin

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WubbaLubStore.Models;
 
 namespace WubbaLubStore.Controllers
 {
@@ -11,5 +13,20 @@ namespace WubbaLubStore.Controllers
   [ApiController]
   public class LocationController : ControllerBase
   {
+    public DatabaseContext db { get; set; } = new DatabaseContext();
+
+    [HttpGet]
+    public async Task<ActionResult<List<Location>>> GetAllLocations()
+    {
+      return await db.Locations.OrderBy(o => o.Address).ToListAsync();
+    }
+    [HttpPost]
+    public async Task<ActionResult<Location>> CreateNewLocation(Location location)
+    {
+      await db.Locations.AddAsync(location);
+      await db.SaveChangesAsync();
+      return Ok(location);
+    }
+
   }
 }
