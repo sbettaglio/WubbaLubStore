d75e4f4be754ad1de78638de009d75a519011d84
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-05 16:06:29 -0500

    completed explorer deployed to heroku

--- Models/Location.cs
+++ Models/Location.cs
@@ -4,7 +4,7 @@ namespace WubbaLubStore.Models
 {
   public class Location
   {
-    public int? Id { get; set; }
+    public int Id { get; set; }
     public string Address { get; set; }
     public string ManagerName { get; set; }
     public string PhoneNumber { get; set; }
