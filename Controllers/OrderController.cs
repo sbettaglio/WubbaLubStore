d99537f6365e93b56042d5b73391f211a1cf35cd
Stefan Bettaglio <sbettaglio@gmail.com>
2020-03-06 12:58:59 -0500

    add and remove items from an order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -20,6 +20,22 @@ namespace WubbaLubStore.Controllers
     {
       return await db.Orders.OrderBy(o => o.Id).ToListAsync();
     }
+    [HttpGet("items/{orderNumber}")]
+    public async Task<ActionResult<List<Order>>> GetAllItemsInOrder(int orderNumber)
+    {
+      var orderExist = db.Orders.Any(o => o.OrderNumber == orderNumber);
+      if (orderExist == true)
+      {
+        var itemsInOrder = db.Orders.Where(o => o.OrderNumber == orderNumber);
+        return await itemsInOrder.ToListAsync();
+      }
+      else
+      {
+        return Ok(new { message = "That order is not in our system" });
+      }
+
+
+    }
 
     [HttpPost("{itemId}")]
     public async Task<ActionResult<List<Order>>> CreateNewOrder(Order order, int itemId)
@@ -45,6 +61,38 @@ namespace WubbaLubStore.Controllers
         return Ok(order);
       }
     }
+    [HttpPatch("update/{Id}/add/{amountAdded}")]
+    public async Task<ActionResult<Order>> AddToOneOrder(int id, int amountAdded)
+    {
+      var orderAmountUpdate = await db.Orders.FirstOrDefaultAsync(o => o.Id == id);
+      var itemOrderLink = await db.ItemOrders.FirstOrDefaultAsync(i => i.OrderId == id);
+      var itemInStock = await db.Items.FirstOrDefaultAsync(s => s.Id == itemOrderLink.ItemId);
+      if (amountAdded > itemInStock.NumberInStock)
+      {
+        return Ok(new { message = "Inventory is not sufficient to fulfill this order. Please enter a lower amount" });
+      }
+      else
+      {
+        itemInStock.NumberInStock -= amountAdded;
+        orderAmountUpdate.AmountOrdered += amountAdded;
+        await db.SaveChangesAsync();
+        return Ok(orderAmountUpdate.AmountOrdered);
+      }
+    }
+    [HttpPatch("update/{Id}/remove/{amountRemoved}")]
+    public async Task<ActionResult<Order>> RemoveFromOneOrder(int id, int amountRemoved)
+    {
+      var orderAmountUpdate = await db.Orders.FirstOrDefaultAsync(o => o.Id == id);
+      var itemOrderLink = await db.ItemOrders.FirstOrDefaultAsync(i => i.OrderId == id);
+      var itemInStock = await db.Items.FirstOrDefaultAsync(s => s.Id == itemOrderLink.ItemId);
+
+      itemInStock.NumberInStock += amountRemoved;
+      orderAmountUpdate.AmountOrdered -= amountRemoved;
+      await db.SaveChangesAsync();
+      return Ok(orderAmountUpdate);
+
+    }
+
 
 
 
